5cc2498351ccb190098858761decae9a126b1690
SUBHODEEP MOITRA <64082425+subhodeepmoitra@users.noreply.github.com>
2021-01-18 12:09:08 +0530

    Add files via upload

--- covid_vaccine_administration_date.cpp
+++ covid_vaccine_administration_date.cpp
@@ -26,7 +26,7 @@ class Vaccination_details : virtual public Patient_details{
         char vaccine_name[100];
         char manufacturer[100];
         char date_of_manufacturing[10];
-        char date_of_administration[10];
+        
     public:
         void Vaccine_details(){
             cout << "\nName of Vaccine: ";
@@ -35,8 +35,6 @@ class Vaccination_details : virtual public Patient_details{
             cin >> manufacturer;
             cout << "\nDate of manufacturing(dd-mm-yyyy): ";
             cin >> date_of_manufacturing;
-            cout << "\nDate of administration(dd-mm-yyyy): ";
-            cin >> date_of_administration;
         }
 };
 class Administration_details : virtual public Patient_details{
@@ -63,7 +61,59 @@ class Print_details : public Administration_details , public Vaccination_details
            cout << "\nVaccine candidate administered: " << vaccine_name << endl;
            cout << "\nName of person who administered: " << name_of_doctor << endl;
        }
+       
+
 };
+    void increase_date(int dd, int mm, int yyyy){
+    int new_month, new_year, temp_month , total_date , actual_date , remaining_days;
+    if ((mm==1)||(mm==3)||(mm==5)||(mm==7)||(mm==8)||(mm==10)||(mm==12)){
+        total_date = dd + 28;
+        if (total_date > 31){
+            if (mm==1){
+                actual_date = total_date - 28;
+                if (actual_date > 28){
+                    int new_date = actual_date - 2;
+                    cout << "The next vaccine date is: " << new_date << "-" << mm+1 << "-" << yyyy << endl;
+                }
+                else if (actual_date == 31){
+                    cout << "The next vaccination date is: 1-3-2021" << endl;
+                }
+            }
+            else {
+                actual_date = total_date - 31;
+                cout << "The next vaccine date is: " << actual_date << "-" << mm+1 << "-" << yyyy << endl;
+            }
+        }
+        else {
+            cout << "The next date is: " << total_date << endl;
+            cout << "The next vaccine date is: " << total_date << "-" << mm << "-" << yyyy << endl;
+        }
+    }
+    else if ((mm==4)||(mm==6)||(mm==9)||(mm==11)){
+        total_date = dd + 28;
+        if (total_date > 30){
+            actual_date = total_date - 30;
+            cout << "The next date is: " << actual_date << endl;
+            cout << "The next vaccine date is: " << actual_date << "-" << mm+1 << "-" << yyyy << endl;
+        }
+        else {
+            cout << "The next date is: " << total_date << endl;
+            cout << "The next vaccine date is: " << total_date << "-" << mm << "-" << yyyy << endl;
+        }
+    }
+    else if (mm==2){
+        total_date = dd + 28;
+        if (total_date > 28){
+            actual_date = total_date - 28;
+            cout << "The next date is: " << actual_date << endl;
+            cout << "The next vaccine date is: " << actual_date << "-" << mm+1 << "-" << yyyy << endl;
+        }
+        else {
+            cout << "The next date is: " << total_date << endl;
+            cout << "The next vaccine date is: " << total_date << "-" << mm << "-" << yyyy << endl;
+        }
+    }
+}
 int main(){
     Print_details obj;
     cout << "-------------- ENTER PATIENT DETAILS --------------\n";
@@ -74,5 +124,13 @@ int main(){
     obj.Other_necessary_details();
     cout << "--------------- DISPLAYING NECESSARY DETAILS ------------------\n";
     obj.Display_data();
+    int dd, mm, yyyy;
+    cout << "Date of administration: ";
+    cin >> dd;
+    cout << "Month of administration: ";
+    cin >> mm;
+    cout << "Year of administration: ";
+    cin >> yyyy;
+    increase_date(dd,mm,yyyy);
     return 0;
 }
